1918534fcab891890b3bd936437b42fe22e24de1
davidbaquero <davidsanti.programador@gmail.com>
2025-04-03 07:51:43 -0500

    solucion_ejericios_evaluacion_colecciones

--- Fundamentos del Lenguaje/solucion_evaluacion_colecciones/colecciones5.py
+++ Fundamentos del Lenguaje/solucion_evaluacion_colecciones/colecciones5.py
@@ -5,21 +5,21 @@ datos = [5, 4, 3, 2, 1]
 print(
     f"Este es el menú de opciones que tienes con estos datos.\n{datos}\n1. Calcular promedio\n2. Identificar el valor máximo\n3. Identificar el valor mínimo\n4. Eliminar un número")
 
-while True:  
+while True:
     opcion = input("Elige una opción para el menú: ")
 
-    if opcion == "1":  
+    if opcion == "1":
         promedio = sum(datos) / len(datos)
         print(f"El promedio de los números es: {promedio}")
-        
+
     elif opcion == "2":
         maximo = max(datos)  # Encuentra el número más alto en la lista
         print(f"El valor máximo es: {maximo}")
-        
+
     elif opcion == "3":
         minimo = min(datos)  # Encuentra el número más alto en la lista
         print(f"El valor minimo es: {minimo}")
-        
+
     elif opcion == "4":
         num = int(input("Ingresa el número que deseas eliminar: "))
         if num in datos:  # Verifica si el número está en la lista
@@ -30,5 +30,3 @@ while True:
             print("El número no está en la lista.")
     else:
         print("el numero no se encuentra en la lista.")
-
-        
