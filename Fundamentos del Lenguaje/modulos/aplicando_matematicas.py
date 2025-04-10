95e65b822329f2b408953ccc13b5278e5d2eacb4
sebastianmanriquetorres <sebastian.manrique.torres@gmail.com>
2025-04-10 17:05:21 -0500

    aplicando modulos matematicas

---
+++ Fundamentos del Lenguaje/modulos/aplicando_matematicas.py
@@ -0,0 +1,10 @@
+import matematicas
+
+# Solicitar al usuario que ingrese los valores
+a = float(input("Ingresa el primer número: "))
+b = float(input("Ingresa el segundo número: "))
+
+print("Suma:", matematicas.sumar(a, b))
+print("Resta:", matematicas.restar(a, b))
+print("Multiplicación:", matematicas.multiplicar(a, b))
+print("División:", matematicas.dividir(a, b))
