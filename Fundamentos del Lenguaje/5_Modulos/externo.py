8afd564c87f46a98eb25fb280afb2477d2bfde83
James <rendondavid328@gmail.com>
2025-04-11 14:30:58 -0500

    cambios

---
+++ Fundamentos del Lenguaje/5_Modulos/externo.py
@@ -0,0 +1,6 @@
+import matematicas
+
+print(matematicas.sumar(5, 6))
+print(matematicas.restar(5, 6))
+print(matematicas.Multiplicar(5, 6))
+print(matematicas.dividir(5, 9))
---
+++ Fundamentos del Lenguaje/5_Modulos/matematicas.py
@@ -0,0 +1,13 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def Multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        print("No puede realizar la division")
+    else: return a / b
