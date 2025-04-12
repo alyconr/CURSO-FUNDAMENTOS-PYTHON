3bd3f0eaf3e7ecee489f457700e31aa8101ea148
Will <166151265+00RAM00@users.noreply.github.com>
2025-04-12 07:44:30 -1200

    Apropiacion de modulos

---
+++ modulo/programa_matematica.py
@@ -0,0 +1,14 @@
+import matematicas
+
+num1 = 10
+num2 = 5
+
+print(f"Suma: {matematicas.suma(num1, num2)}")
+print(f"Resta: {matematicas.resta(num1, num2)}")
+print(f"Multiplicación: {matematicas.multiplicacion(num1, num2)}")
+print(f"División: {matematicas.division(num1, num2)}")
+
+num3 = 8
+num4 = 0
+
+print(f"División: {matematicas.division(num3, num4)}")
