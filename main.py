4143ad3216ed9f159db6ac4080102c70b7f89a8d
David435412 <davidm435465123@gmail.com>
2025-04-12 16:42:38 -0500

    subo ejercicios de modulos

---
+++ main.py
@@ -0,0 +1,10 @@
+import matematicas
+
+# Probamos las funciones con dos números
+a = 20
+b = 7
+
+print("Suma:", matematicas.suma(a, b))
+print("Resta:", matematicas.resta(a, b))
+print("Multiplicación:", matematicas.multiplicacion(a, b))
+print("División:", matematicas.division(a, b))
