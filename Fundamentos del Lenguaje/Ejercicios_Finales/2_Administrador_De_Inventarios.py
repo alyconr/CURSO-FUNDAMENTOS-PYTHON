4715c1e50f2dad28d7eab5b31091bf8f65be35ca
James <rendondavid328@gmail.com>
2025-04-19 11:57:53 -0500

    cambios

---
+++ Fundamentos del Lenguaje/Ejercicios_Finales/2_Administrador_De_Inventarios.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 2: Administrador de Inventarios
+Descripción:
+Desarrolla un sistema para gestionar el inventario de productos usando un diccionario donde cada clave es el nombre del producto y su valor la cantidad en stock.
+Implementa funciones para agregar, actualizar y eliminar productos.
+Utiliza estructuras de control para recorrer y mostrar el inventario.
+Incluye manejo de excepciones para capturar intentos de actualizar o eliminar un producto inexistente.
+Ejemplo de salida:
+Inventario inicial: {'manzanas': 50, 'naranjas': 30, 'peras': 20} Actualizando stock de 'peras' a 25... Producto 'bananas' agregado con 40 unidades. Eliminando 'naranjas'... Inventario final: {'manzanas': 50, 'peras': 25, 'bananas': 40}
+"""
+
