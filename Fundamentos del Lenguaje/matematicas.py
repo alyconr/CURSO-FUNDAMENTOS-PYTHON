31838293246532e00f98843424f38c8372972282
BrandonQuinte <brandonquintero028@gmail.com>
2025-04-20 17:04:57 -0500

    Subiendo ejercicios finales en Python

---
+++ Fundamentos del Lenguaje/matematicas.py
@@ -0,0 +1,15 @@
+##Cree un módulo llamado matematicas.py con las funciones básicas suma, resta, multiplicacion y division. Todas estas funciones reciben dos números y retornan el respectivo resultado.
+
+#Cree un programa en python que haga uso de cada una de las funciones que se encuentran en el módulo matematicas
+
+def suma (x,y):
+    return x+y
+
+def resta (x,y):
+    return x-y
+
+def multiplicacion (x,y):
+    return x*y
+
+def division (x,y):
+    return x/y
---
+++ Fundamentos del Lenguaje/resultados.py
@@ -0,0 +1,6 @@
+from matematicas import *
+
+print(suma(4,9))
+print(resta(10,11))
+print(multiplicacion(6,9))
+print(division(20,10))
