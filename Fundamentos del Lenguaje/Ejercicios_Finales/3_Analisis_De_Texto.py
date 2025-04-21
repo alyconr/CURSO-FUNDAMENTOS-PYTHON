8d8998ec57423bcdf0716ec29bf2b5bc1ff966f4
James <rendondavid328@gmail.com>
2025-04-20 20:27:12 -0500

    Cambios

---
+++ Fundamentos del Lenguaje/Ejercicios_Finales/3_Analisis_De_Texto.py
@@ -0,0 +1,10 @@
+"""
+Descripción:
+Crea una función que reciba una cadena de texto y calcule la frecuencia de cada palabra en el mismo.
+
+Convierte el texto a minúsculas y separa las palabras usando algún método (puedes utilizar expresiones regulares con el módulo re).
+Emplea un diccionario para almacenar la palabra (clave) y su frecuencia (valor).
+Controla excepciones en caso de que el usuario no ingrese ningún texto o ingrese caracteres inesperados.
+Ejemplo de salida:
+Ingrese un texto: "Hola mundo, hola a todos en el mundo" Frecuencia de palabras: hola: 2 mundo: 2 a: 1 todos: 1 en: 1 el: 1
+"""
