97b484a83a57b44356d8a0bc8fc357122fd98190
JuanPabloBeltran <juanpablobeltranrodriguez@gmail.com>
2025-04-23 16:07:22 -0500

    Nuevos cambios

---
+++ solucion/Creacióndemodulos_Apropiación/programacalculadora.py
@@ -0,0 +1,10 @@
+import matematicas
+
+a = int(input("Ingresa el primer número: "))
+b = int(input("Ingresa el segundo número: "))
+
+
+print("Suma:", matematicas.sumar(a, b))
+print("Resta:", matematicas.restar(a, b))
+print("Multiplicación:", matematicas.multiplicar(a, b))
+print("División:", matematicas.dividir(a, b))
